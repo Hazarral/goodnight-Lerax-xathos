8db51f48887c0d650b16463da64d8f86a74b0ac9
Hazarral <huynhquochuy2006@gmail.com>
2026-08-10 21:13:54 +0700

    added water shield

--- test.gd
+++ test.gd
@@ -1,29 +1,54 @@
 extends CanvasLayer
 
+## Shields
 @onready var fire_shield_label := $VBoxContainer/Shields/FireGroup/FireShieldLabel
 @onready var fire_shield_bar := $VBoxContainer/Shields/FireGroup/FireShield
-@onready var hp_bar := $VBoxContainer/HPBar
-@onready var hp_label := $VBoxContainer/HPBar/Label
-@onready var attack_button := $VBoxContainer/Button
-
 @export var max_fire_shield := 500
 @export var current_fire_shield := max_fire_shield
+
+@onready var water_shield_label := $VBoxContainer/Shields/WaterGroup/WaterShieldLabel
+@onready var water_shield_bar := $VBoxContainer/Shields/WaterGroup/WaterShield
+@export var max_water_shield := 700
+@export var current_water_shield := max_water_shield
+
+@onready var wind_shield_label := $VBoxContainer/Shields/WindGroup/WindShieldLabel
+@onready var wind_shield_bar := $VBoxContainer/Shields/WindGroup/WindShield
+@export var max_wind_shield := 1000
+@export var current_wind_shield := max_wind_shield
+
+## HP
+@onready var hp_bar := $VBoxContainer/HPBar
+@onready var hp_label := $VBoxContainer/HPBar/Label
 @export var max_hp := 1000
 @export var current_hp := max_hp
-@export var damage := 75
-
 var is_dead := false
 
+## Attacks
+@onready var fire_attack_button := $VBoxContainer/AttackButtons/FireAttack
+@export var fire_damage := 75
+
+@onready var water_attack_button := $VBoxContainer/AttackButtons/WaterAttack
+@export var water_damage := 40
+
+## Text
 const FIRE_SHIELD_TEXT := "%d/%d Fire Shield"
+const WATER_SHIELD_TEXT := "%d/%d Water Shield"
 const HP_LABEL_TEXT := "%d/%d HP"
 
 func _ready() -> void:
-	fire_shield_bar.max_value = max_fire_shield
-	hp_bar.max_value = max_hp
+	setup_shield_and_hp()
 	
-	update_shield_bar()
+	update_shield_bar(DamageAndDot.DamageType.FIRE)
+	update_shield_bar(DamageAndDot.DamageType.WATER)
 	update_hp_bar()
-	attack_button.text = "Attack: %d damage" % damage
+	
+	fire_attack_button.text = "Attack: %d Fire damage" % fire_damage
+	water_attack_button.text = "Attack: %d Water damage" % water_damage
+
+func setup_shield_and_hp() -> void:
+	fire_shield_bar.max_value = max_fire_shield
+	water_shield_bar.max_value = max_water_shield
+	hp_bar.max_value = max_hp
 
 func update_hp_bar() -> void:
 	if is_dead:
@@ -34,31 +59,62 @@ func update_hp_bar() -> void:
 	hp_label.text = HP_LABEL_TEXT % [current_hp, max_hp]
 	hp_bar.value = current_hp
 
-func update_shield_bar() -> void:
+func update_shield_bar(damage_type : DamageAndDot.DamageType) -> void:
 	if is_dead:
 		print("Target already dead! Shield is of no use.")
 		return
 	
-	fire_shield_label.text = FIRE_SHIELD_TEXT % [current_fire_shield, max_fire_shield]
-	fire_shield_bar.value = current_fire_shield
+	match damage_type:
+		DamageAndDot.DamageType.FIRE:
+			fire_shield_label.text = FIRE_SHIELD_TEXT % [current_fire_shield, max_fire_shield]
+			fire_shield_bar.value = current_fire_shield
+		DamageAndDot.DamageType.WATER:
+			water_shield_label.text = WATER_SHIELD_TEXT % [current_water_shield, max_water_shield]
+			water_shield_bar.value = current_water_shield
+		_:
+			print("Other damage not implemented!")
+			return
 
-func _on_button_pressed() -> void:
+func damage(damage_type : DamageAndDot.DamageType, damage : int) -> void:
 	if is_dead:
 		print("Target already dead! You can absorb them instead.")
 		return
 	
-	var damage_to_shield := mini(current_fire_shield, damage)
-	current_fire_shield -= damage_to_shield
-	current_hp = maxi(current_hp - (damage - damage_to_shield), 0)
+	## 1. Hit the shield first
+	var damage_to_shield : int = 0
+	match damage_type:
+		DamageAndDot.DamageType.FIRE:
+			damage_to_shield = mini(current_fire_shield, damage)
+			if damage_to_shield > 0 and current_fire_shield <= 0:
+				print("Fire shield broken!")
+			
+			current_fire_shield -= damage_to_shield
+			update_shield_bar(DamageAndDot.DamageType.FIRE)
+		DamageAndDot.DamageType.WATER:
+			damage_to_shield = mini(current_water_shield, damage)
+			if damage_to_shield > 0 and current_water_shield <= 0:
+				print("Water shield broken!")
+			
+			current_water_shield -= damage_to_shield
+			update_shield_bar(DamageAndDot.DamageType.WATER)
+		_:
+			print("Other damage not implemented!")
+			return
 	
-	if damage_to_shield > 0:
-		update_shield_bar()
-		if current_fire_shield <= 0:
-			print("Fire shield broken!")
+	## 2. The surplus will go into HP
+	# This is guaranteed to be non-negative
+	var damage_to_hp := damage - damage_to_shield 
 	
+	current_hp = maxi(0, current_hp - damage_to_hp)
 	if current_hp <= 0:
-		current_hp = 0 # Clamp nicely
-		is_dead = true
 		print("Target killed!")
+		is_dead = true
 	
 	update_hp_bar()
+	
+func _on_fire_attack_pressed() -> void:
+	damage(DamageAndDot.DamageType.FIRE, fire_damage)
+	print("Called damage function for fire")
+
+func _on_water_attack_pressed() -> void:
+	damage(DamageAndDot.DamageType.WATER, water_damage)
